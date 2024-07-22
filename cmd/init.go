fb209c5e9a0c8526b0282c0795905d64fe2e4e6e
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-07-22 13:54:10 +0545

    feat: add initCmd implementation

---
+++ cmd/init.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"bruno-init-suite/internal/initializer"
+	"fmt"
+	"github.com/spf13/cobra"
+	"path/filepath"
+)
+
+var useCognito bool
+var projectName string
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize a new Bruno project",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Absolute path for outputDir
+		absOutputDir, err := filepath.Abs(outputDir)
+		if err != nil {
+			return fmt.Errorf("failed to determine absolute path: %w", err)
+		}
+
+		err = initializer.Initialize(absOutputDir, projectName, useCognito)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Files generated successfully!")
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+	// Add flags to the init command
+	// The flag useCognito is used to determine if the project will use AWS Cognito for authentication
+	initCmd.Flags().BoolVarP(&useCognito, "cognito", "", false, "Use AWS Cognito for authentication")
+	// The flag for name is used to set the project name
+	initCmd.Flags().StringVarP(&projectName, "name", "n", "bruno-docs", "Name of the project")
+}
