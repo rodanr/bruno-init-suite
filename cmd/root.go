b54a59ab760ac0183ed4ee628de8f5212172c5cc
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-07-22 12:58:20 +0545

    feat: setup root command

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "bruis", // short form for bruno init suite
+	Short: "CLI tool for generating bruno docs with custom configurations",
+	Long:  `A CLI tool to automate the creation of Bruno docs and integration with third part services, including scripts and configurations`,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Printf("Oops. An error occurred while executing Bruno Init Suite '%s'\n", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
